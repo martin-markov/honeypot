9fd902277142530fed7d2a3605124718fc75c3c9
martin-markov <inj.martin.markov@gmail.com>
2018-08-11 18:10:22 +0300

    addtional files

---
+++ DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Honeypot.Filter;
+using Honeypot.Helpers;
+
+namespace DemoApp
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //[HoneypotFilter("HolderName", "ValidDate", "CardNumber", "SecurityCode")]
+        //public ActionResult PostCreditCardData(CreditCard model)
+        //{
+        //    if (Request.Form.AllKeys.Contains("HasHoneypotTrapped"))
+        //    {
+        //        return Json("/Home/BotPage");
+        //    }
+        //    else
+        //    {
+        //        return View("~/Views/Home/ThankYou.cshtml");
+        //    }
+        //}
+        [HttpPost]
+        [HoneypotFilter(typeof(CreditCard))]
+        public ActionResult PostCreditCardData(CreditCard model)
+        {
+            if (HttpContext.Request.HasHoneypotTrapped())
+            {
+                return Json("/Home/BotPage");
+
+            }
+            else
+            {
+                return View("~/Views/Home/ThankYou.cshtml");
+            }
+        }
+
+        public ActionResult BotPage()
+        {
+            return View();
+        }
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DemoApp/Models/CreditCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp
+{
+    public class CreditCard
+    {
+        public string HolderName { get; set; }
+        public string ValidDate { get; set; }
+        public string CardNumber { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
